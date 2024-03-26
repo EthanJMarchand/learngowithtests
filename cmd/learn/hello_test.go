365f2ef2789b7be8cd46f18a07681b3e6a7fbeca
Ethan Marchand <ethanjmarchand@gmail.com>
2024-03-26 16:01:32 -0600

    added my project thus far

---
+++ cmd/learn/hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Ethan")
+	want := "Hello, Ethan"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
