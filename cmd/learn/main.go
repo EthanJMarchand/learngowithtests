d961bbd5f1420027771a432e6da0d94678d94505
Ethan Marchand <ethanjmarchand@gmail.com>
2024-03-26 16:38:26 -0600

    Testing to see if git will add my handlers package inside my internal folder

--- cmd/learn/main.go
+++ cmd/learn/main.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	spanishHelloPrefix = "Hola, "
+	englishHelloPrefix = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return prefix(language) + name
+}
+
+func prefix(language string) (prefix string) {
+	switch language {
+	case "spanish":
+		prefix = spanishHelloPrefix
+	case "french":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Ethan"))
+	fmt.Println(Hello("Ethan", "english"))
 }
